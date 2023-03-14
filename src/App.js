9fd2fdd0049cc185bd6b805a291de1927437730c
Veervijay <virajrana123@gmail.com>
2023-03-14 13:50:31 -0400

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router";
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
@@ -7,8 +7,15 @@ import Resume from "./Pages/Resume/Resume";
 import NavbarComponent from "./Components/Navbar/NavbarComponent";
 import "./style.css";
 import Footer from "./Components/Navbar/Footer";
+import ReactGA from "react-ga";
 
+const trackingId = "UA-194287070-1";
+ReactGA.initialize(trackingId);
 function App() {
+  useEffect(() => {
+    // This will trigger the first pageview event when the component mounts
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <React.Fragment>
       <NavbarComponent />
